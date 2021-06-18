2d33a2e2cc76e2c30d8bcbdebace4543a4bfbeb0
Lucas Evangelista <lucas.evangelistadev@gmail.com>
2021-06-18 10:10:52 -0300

    ajustes

--- admin_page.php
+++ admin_page.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
     <title>Admin</title>
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-    <link rel="stylesheet" href="css/admin.css">
+    <link rel="stylesheet" href="css/admin_page.css">
 </head>
 
 <body>
